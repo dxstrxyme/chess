a2de110a986ecdb8a7a3b0c343d6feda399c1744
nikita <dxstrxyme@gmail.com>
2025-08-02 16:02:05 +0300

    Added rules for move figures

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -13,9 +13,12 @@ const BoardComponent:FC<BoardProps> = ({board , setBoard})=> {
   const [selectedCell,setSelectedCell] = useState<Cell | null>(null);
 
   function click(cell:Cell){
-    if(cell.figure){
+    if(selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)){
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+    } else {
       setSelectedCell(cell);
-    } 
+    }
   }
 
   useEffect(()=>{
