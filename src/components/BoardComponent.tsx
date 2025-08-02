4191e68017a4c3c891bbeebbee3b0042e6031edc
nikita <dxstrxyme@gmail.com>
2025-08-02 15:22:33 +0300

    Added function to show selected cell

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -11,12 +11,19 @@ interface BoardProps{
 
 const BoardComponent:FC<BoardProps> = ({board , setBoard})=> {
   const [selectedCell,setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell:Cell){
+    if(cell.figure){
+      setSelectedCell(cell);
+    } 
+  }
   return (
     <div className='board'>
       {board.cells.map((row,index)=>
       <React.Fragment key={index}>
         {row.map(cell=>
           <CellComponent 
+          click={click}
           cell={cell}
           key={cell.id}
           selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}/>
